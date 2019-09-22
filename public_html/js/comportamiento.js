1cc1524536542887eb7b17a5807c97db5edd8eef
JIN26 <Angelmantilla26@gmail.com>
2019-09-22 18:26:58 -0500

    progrma terminado

--- public_html/js/comportamiento.js
+++ public_html/js/comportamiento.js
@@ -1,12 +1,51 @@
+var vp = document.getElementById("Mapa");
+var papel = vp.getContext("2d");
 
-var base = document.getElementById("Mapa").getContext("2d");
-var mapa = Imagenes/Mapa.jpg;
-var imagen = new Image(); 
-imagen.src = mapa;
-console.log(base);
-imagen.addEventListener("load", dibujar);
+var fondo = {
+  url: "js/Imagenes/mapa.jpg",
+  cargaOK: false
+};
+
+var azul = {
+  url: "js/Imagenes/Azul_1.png",
+  cargaOK: false
+};
+
+var cantidad = aleatorio(1, 10);
+
+fondo.imagen = new Image(); 
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+azul.imagen = new Image(); 
+azul.imagen.src = azul.url;
+azul.imagen.addEventListener("load", cargarAzul);
+
+function cargarFondo(){
+  fondo.cargaOK = true;
+  dibujar();
+}
+function cargarAzul(){
+  azul.cargaOK = true;
+  dibujar();
+}
+
 function dibujar(){
-    base.drawImage(imagen,0,0);
+    
+  if(fondo.cargaOK){
+    papel.drawImage(fondo.imagen, 0, 0);
+  }
+  
+  if(azul.cargaOK){
+    console.log(cantidad);
+    for(var i=0; i < cantidad; i++){
+      var x = aleatorio(0, 7);
+      var y = aleatorio(0, 10);
+      var x = x * 60;
+      var y = y * 40;
+      papel.drawImage(azul.imagen, x, y);
+    }
+  }
 }
 
 function aleatorio(min, max){
